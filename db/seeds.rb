89ed9a7356d918cdb567487e216ea8d565e4df09
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-31 15:08:38 -0800

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# t.string "type"
+# t.string "name"
+# t.string "color"
+# t.integer "age"
+
+
+
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_quotes
+  end
+  def generate_quotes
+    10.times do |i|
+      animal = Animal.create!(
+      type: Faker::Creature::Dog.breed,
+      name: Faker::Creature::Dog.name,
+      color: Faker::Color.color_name,
+
+
+      )
+
+  end
+end
