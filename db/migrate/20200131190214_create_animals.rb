a60960f8e2627c7862606955137c8c7105167af4
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-31 12:23:06 -0800

    roll back database and create a new one

--- db/migrate/20200131190214_create_animals.rb
+++ db/migrate/20200131190214_create_animals.rb
@@ -1,6 +1,11 @@
 class CreateAnimals < ActiveRecord::Migration[6.0]
   def change
     create_table :animals do |t|
+      t.string :type
+      t.string :name
+      t.string :color
+      t.integer :age
+      t.timestamps()
     end
   end
 end
