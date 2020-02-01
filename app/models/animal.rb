2e814ed6d675e440c3c5e3369b0a3c247174ca9d
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2020-01-31 17:03:07 -0800

    add search scope

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,15 @@
 class Animal < ApplicationRecord
+  validates :breed, :name, :color, :age, :presence => true
+
+  def self.search_name(name)
+      where("name like ?", "%#{name.titleize}%")
+  end
+
+  def self.search_name(breed)
+      where("name like ?", "%#{breed.titleize}%")
+  end
+  
+  def self.search_name(color)
+      where("name like ?", "%#{color.titleize}%")
+  end
 end
